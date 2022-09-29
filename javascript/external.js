ef6a7b27b588f512c8ee71a14949bc436dd5160c
Griffin Moore 9/29 <goc10@txstate.edu>
2022-09-29 12:05:17 -0500

    added step 3

--- javascript/external.js
+++ javascript/external.js
@@ -9,8 +9,8 @@
 //A prompt box is used to prompt users to input a value before entering a page.
 
 
-user_name = window.prompt("Please enter your name", "Type your name here");
-document.write(user_name);
+//user_name = window.prompt("Please enter your name", "Type your name here");
+//document.write(user_name);
 
 
 
@@ -34,3 +34,16 @@ document.write(user_name);
 {
 // document.writeln(arr[i],"<br>");
 }
+//Start of lab 4
+function welcome()
+{
+ let a = "Please enter your name.";
+ let b = "Type your name here.";
+ // A prompt box is used to prompt users to input a value
+before entering a page.
+ user_name = window.prompt(a, b);
+ message = "<h1>Hello, welcome to my webpage, " + user_name
++ "!</h1>"
+ return message
+}
+document.write(welcome);
