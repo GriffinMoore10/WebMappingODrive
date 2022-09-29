682afa473b8f03096fd0bb841d5da05fa9276c70
Griffin Moore 9/29 <goc10@txstate.edu>
2022-09-29 12:02:35 -0500

    pasted js

---
+++ javascript/external.js
@@ -0,0 +1,36 @@
+//document.write("Where is my JS code?");
+//window.alert(5 + 6);
+//window.alert(‘5 + 6’);
+//document.write(‘<h1>A heading</h1>’);
+//document.write(‘<p>A sentence.</p>’);
+//const x = "web";
+//const out = x + y;
+//document.write(out);
+//A prompt box is used to prompt users to input a value before entering a page.
+
+
+user_name = window.prompt("Please enter your name", "Type your name here");
+document.write(user_name);
+
+
+
+//document.writeln("<button onclick='condition()'>Conditional
+//Test</button>")
+//function condition()
+{
+ //x = confirm("Are you sure you want to proceed?");
+ //if(x)
+ {
+ //document.writeln("You chose Okay!");
+ }
+ //else
+ {
+// document.writeln("You chose Cancel!");
+ }
+}
+
+//arr = ["Mael","Eric","Randall","James"];
+//for (var i=0; i < arr.length; i++)
+{
+// document.writeln(arr[i],"<br>");
+}
