5cc5038e9d39c04963ba1cbe0d733cd90f43b61f
Griffin Moore 9/29 <goc10@txstate.edu>
2022-09-29 13:18:11 -0500

    fixed let

--- javascript/external.js
+++ javascript/external.js
@@ -38,13 +38,11 @@ function welcome()
 {
  let a = "Please enter your name.";
  let b = "Type your name here.";
- // A prompt box is used to prompt users to input a value
-before entering a page.
+ // A prompt box is used to prompt users to input a value before entering a page.
  user_name = window.prompt(a, b);
- message = "<h1>Hello, welcome to my webpage, " + user_name
+ let message = "<h1>Hello, welcome to my webpage, " + user_name
 + "!</h1>"
  return message
 }
 document.write(welcome);
-document.write(message);message = "<h1>Hello, welcome to my webpage, " + user_name +
-"!</h1>"
+document.write(let message);
